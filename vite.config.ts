050a31419b1da627e71197cc3803eaf7f78ef3e1
Maulen <mac@MacBook-Air-mak.Dlink>
2023-08-19 21:04:20 +0600

    [инфо] оптимизация картинок

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import image from '@rollup/plugin-image';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      ...image(),
+      enforce: 'pre',
+    },
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
